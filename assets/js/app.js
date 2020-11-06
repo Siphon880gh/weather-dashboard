d2ad38008f501522b4c8d9f03f08ad31a79f10ed
Weng Fei Fung <weffung@ucdavis.edu>
2020-11-06 06:48:41 -0800

    Add fetch weather stats for current day

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,99 @@
+/**
+ * @dependency MomentJS, quickTester
+ */
+
 console.log("JS Connected");
+
+let agency = function(published, subscriber) {
+    subscriber(published);
+}
+let utility = {
+    getTodaysDate() {
+        return moment().format("MM/DD/YYYY");
+    }
+}
+/**
+ * @object app
+ * TO REVIEW: Attempted MVC and publisher subscriber pattern (if I understood it correctly). Come back to this after learning MVC and publisher/subscriber
+ * 
+ * @method getWeather Limitation: Querying their API with City name does not return 5 day forecast and UV index
+ * @returns {object} Includes longitude and latitude
+ * {"coord":{"lon":-118.24,"lat":34.05},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"base":"stations","main":{"temp":289.51,"feels_like":288.87,"temp_min":289.15,"temp_max":290.15,"pressure":1007,"humidity":82},"visibility":10000,"wind":{"speed":2.38,"deg":124},"clouds":{"all":40},"dt":1604671262,"sys":{"type":1,"id":4361,"country":"US","sunrise":1604672262,"sunset":1604710533},"timezone":-28800,"id":5368361,"name":"Los Angeles","cod":200}
+ * 
+ * @method getExpandedWeather Only after getting the longitude and latitude from a city search, can you get the 5 day forecast and UV index by querying their API with longitude and latitude
+ * @returns {object} Includes 5 day forecast and UV index
+ * 
+ */
+var app = {
+    apiKey: "51d896b9c9317052fd630d3fc467c276",
+    models: {
+        init: function() {
+            // $.get("//api.openweathermap.org/data/2.5/weather?q=Los%20Angeles&appid=").then(response=>{ 
+            //     console.log(response);
+            // });
+        },
+        getWeather: function(city) {
+            let apiKey = app.apiKey;
+
+            $.get(`//api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`).then(response=>{ 
+                // Testing
+                if(quickTester.assert(typeof response.coord!=="undefined", "Querying Open Weather with city name failed because it did not return the city coordinates")) debugger;
+
+                let weatherObj = {
+                    today: utility.getTodaysDate(),
+                    temperature: response.main.temp,
+                    humidity: response.main.humidity,
+                    speed: response.wind.speed
+                }
+                let coords = {
+                    lat: response.coord.lat,
+                    lon: response.coord.lon
+                }
+
+                app.models.getExpandedWeather(weatherObj, coords);
+                
+            });
+        },
+        getExpandedWeather: function(weatherObj, coords) {
+            let apiKey = app.apiKey;
+            let {lat, lon} = coords;
+
+            $.get(`//api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`).then(response=>{ 
+                // Testing
+                if(quickTester.assert(typeof response.daily!=="undefined", "Querying Open Weather with longitude and latitude failed because it did not return the daily forecast")) debugger;
+
+                window.test = response;
+                console.dir(response);
+
+                // More properties for weatherObj available after querying by longitude and latitude
+                weatherObj.uvi = response.current.uvi;
+                
+            });
+        }
+    },
+    views: {
+        init: function() {
+        },
+        setWeather: function(weatherObj) {
+            alert(weatherObj);
+        }
+    },
+    controllers: {
+        init: function() {
+
+            $("#search-city-button").on("click", ()=>{
+                // Get text
+                let queryCity = $("#search-city").val();
+                if(!queryCity) {
+                    alert("Error: Please enter a city name to search for weather forecast");
+                    return false;
+                }
+                agency(queryCity, (city)=>{app.models.getWeather(city)})
+            });
+        }
+    }
+}
+
+app.models.init();
+app.views.init();
+app.controllers.init();
